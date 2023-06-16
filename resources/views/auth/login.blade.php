73c3d4db56953a1d744e6437318da99a435e127c
jeffersonxavi <jefingbi2@gmail.com>
2023-06-16 20:57:28 -0300

    atualizando tela login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,64 @@
-@extends('layouts.guest')
+@extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <!-- Email Address -->
-    <div class="form-group">
-        <label for="email">{{ __('Email') }}</label>
-        <input id="email" class="form-control @error('email') is-invalid @enderror" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
-        @error('email')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </div>
-    <!-- Password -->
-    <div class="form-group mt-3">
-        <label for="password">{{ __('Password') }}</label>
-        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" required autocomplete="current-password">
-        @error('password')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </div>
-    <!-- Remember Me -->
-    <div class="form-group form-check mt-3">
-        <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-        <label for="remember_me" class="form-check-label">{{ __('Remember me') }}</label>
-    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Acessar SaúdeTech') }}</div>
 
-    <div class="form-group row mb-0 mt-3">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Log in') }}
-            </button>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-            @if (Route::has('password.request'))
-            <a class="btn btn-link" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-            @endif
+                            <!-- Email Address -->
+                            <div class="form-group">
+                                <label for="email">{{ __('E-mail') }}</label>
+                                <input id="email" class="form-control @error('email') is-invalid @enderror"
+                                    type="email" name="email" :value="old('email')" required autofocus
+                                    autocomplete="username">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <!-- Password -->
+                            <div class="form-group">
+                                <label for="password">{{ __('Senha') }}</label>
+                                <input id="password" class="form-control @error('password') is-invalid @enderror"
+                                    type="password" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <!-- Remember Me -->
+                            <div class="form-group form-check">
+                                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                                <label for="remember_me" class="form-check-label">{{ __('Lembrar-me') }}</label>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Acessar') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Esqueceu sua senha?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</form>
 @endsection
