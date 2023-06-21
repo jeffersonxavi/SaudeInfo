b7c229adcbf2fc7d96f937dd9531adcdfab701ea
jeffersonxavi <jefingbi2@gmail.com>
2023-06-21 00:53:07 -0300

    atualizando link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 
     <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
-    <link rel="stylesheet" href="{{ url('js/app.js') }}">
+    <!-- <link rel="stylesheet" href="{{ url('js/app.js') }}"> -->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
 </head>
@@ -109,6 +109,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="{{ url('js/app.js') }}"></script>
     <script src="{{ url('js/sidebar.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
