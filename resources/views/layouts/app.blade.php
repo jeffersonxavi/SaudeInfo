baee36c4258a01a3b6e3387864112ef2d6ed7e55
jeffersonxavi <jefingbi2@gmail.com>
2023-06-21 20:39:22 -0300

    add cors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,7 +103,7 @@
         @endif
     </main>
     <!-- JavaScript para controlar a barra lateral -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
