66b2f57f3d723a6ce330fef551c3eb2812fb1389
jeffersonxavi <jefingbi2@gmail.com>
2023-06-21 12:10:20 -0300

    instalando vite plugin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 
     <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
-    <link rel="stylesheet" href="{{ url('js/app.js') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
 </head>
@@ -112,6 +112,7 @@
     <script src="{{ url('js/sidebar.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="module" src="{{ url('js/app.js') }}"></script>
     @stack('scripts')
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
