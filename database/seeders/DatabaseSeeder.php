ebc45c1d1d8ae61bb6e4702f7424d79bcb3161a0
jeffersonxavi <jefingbi2@gmail.com>
2023-06-22 19:58:01 -0300

    att factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
    
-        // Paciente::factory()->count(200)->create();
-        // Profissional::factory()->count(10)->create();
+        Paciente::factory()->count(200)->create();
+        Profissional::factory()->count(10)->create();
         Especialidade::factory()->count(10)->create();
         TipoConsulta::factory()->count(5)->create();
         Consulta::factory()->count(30)->create();
